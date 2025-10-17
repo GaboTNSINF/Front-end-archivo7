0fcb149df1bc6178769ab7212e68224bf706ea96
GaboTNSINF <gabrielhernan1261@gmail.com>
2025-10-17 19:19:52 -0300

    Feat: Front-End uwu

---
+++ src/app/interfaces/user.interface.ts
@@ -0,0 +1,10 @@
+export interface User {
+  _id?: string;
+  nombreCompleto: string;
+  email: string;
+  edad?: number;
+  activo?: boolean;
+  fechaCreacion?: string;
+  createdAt?: string;
+  updatedAt?: string;
+}
